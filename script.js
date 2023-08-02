a94c91fa4b03be24516181b5f8c555b530236379
Lucas Anderson <lucas.a@comcast.net>
2023-08-01 23:31:15 -0500

    Add selection logic on starting screen

--- script.js
+++ script.js
@@ -33,6 +33,48 @@ const grid = (() => {
     };
 })();
 
+const start = (() => {
+    let mainContainer = document.getElementById('main-container');
+
+    let startBtn = document.getElementById('start-btn');
+    startBtn.addEventListener('click', () => {
+        beginGame();
+    });
+
+    let iconElements = [];
+    let difElements = [];
+    let iconSelected = -1;
+    let difSelected = -1;
+    for(let i = 0; i < 4; i++) {
+        iconElements.push(document.getElementById('icon-' + i));
+        difElements.push(document.getElementById('dif-' + i));
+        iconElements[i].addEventListener('click', () => {
+            for(let i = 0; i < 4; i++) {
+                iconElements[i].classList.remove('selected');
+            }
+            iconElements[i].classList.add('selected');
+            iconSelected = i
+        });
+        difElements[i].addEventListener('click', () => {
+            for(let i = 0; i < 4; i++) {
+                difElements[i].classList.remove('selected');
+            }
+            difElements[i].classList.add('selected');
+        });
+    }
+
+    const beginGame = () => {
+        while(mainContainer.firstChild!=null) {
+            mainContainer.removeChild(mainContainer.firstChild);
+        }
+        grid.init();
+    }
+
+    return {
+        beginGame,
+    }
+})();
+
 const player = (isPerson, iconFile) => {
 
     return {isPerson, iconFile}
@@ -40,6 +82,10 @@ const player = (isPerson, iconFile) => {
 
 const main = () => {
     let players = [];
+    let curPlayer = true;
 
+    let move = (i) => {
+
+    }
 }
 
